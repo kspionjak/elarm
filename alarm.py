4e4abf0e0dfa78352e7acbfba06798d85a7608e6
kspionjak <kristijan.spionjak@gmail.com>
2024-07-29 14:54:05 +0100

    first working version
    first working version from deb12

--- alarm.py
+++ alarm.py
@@ -1,25 +1,30 @@
-import customtkinter as ctk
+import tkinter as tk
+from tkinter import font as tkFont
 
-class Elam(ctk.CTk):
-  def __init__(self):
-    super().__init__()
+def ok_exit():
+  window.destroy()
 
-    my_font = ctk.CTkFont(family="Tahoma", size=46, weight="bold")
-    
-    self.title("Elam's Alarm!")
-    
-    self.geometry('1024x768+400+400')
-    # self.grid_columnconfigure((0, 1), weight=1)
 
-    self.nameLabel = ctk.CTkLabel(self, 
-                                  text="Ema!!!", 
-                                  text_color="white", 
-                                  fg_color="#01a6f8", 
-                                  width=200,
-                                  height=25,
-                                  corner_radius=5,
-                                  font=my_font)
-    self.nameLabel.pack(pady=30)
+window = tk.Tk()
+window.title("Ema's Alarm!")
 
-app = Elam()
-app.mainloop()
+screen_width =  window.winfo_screenwidth()
+screen_height =  window.winfo_screenheight()
+
+window.geometry("560x400+%d+%d" % (screen_width/2-275, screen_height/2-125))
+# window.eval('tk::PlaceWindow . center')
+
+helv36 = tkFont.Font(family='Helvetica', size=36, weight=tkFont.BOLD)
+
+btn = tk.Button(
+        master=window,
+        text="Dorucak!",
+        font=helv36,
+        fg="white",
+        bg="blue",
+        width=20,
+        height=10,
+        command=ok_exit
+      ).pack()
+
+window.mainloop()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cherrypy
 
 class Elarm(object):
-  @cherrypy.expose
-  def index(self):
-    return "Hello World!"
+    @cherrypy.expose
+    def index(self):
+        return "Hello Elarm!"
 
 if __name__ == "__main__":
-  cherrypy.quickstart(Elarm())
+    cherrypy.config.update({'server.socket_host': '192.168.64.17'})
+    cherrypy.quickstart(Elarm())
+
